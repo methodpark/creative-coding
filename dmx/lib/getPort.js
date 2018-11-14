298bb5c6d6636be2e24395f25c11f97b3963cd2a
Manuel Ernst <manuel.ernst@methodpark.de>
2018-11-14 16:03:21 +0100

    fix port discovery

--- dmx/lib/getPort.js
+++ dmx/lib/getPort.js
@@ -3,16 +3,17 @@ const serialport = require('serialport');
 function getInterfacePort() {
   return new Promise((resolve, reject) => {
     serialport.list((error, ports) => {
+      console.log(ports, error)
       if (error) {
         return reject(error);
       }
     
       const port = ports.find(port => {
-        return port.manufacturer === 'enttec';
+        return port.manufacturer === 'FTDI';
       })
 
       if (port) {
-        resolve(port);
+        resolve(port.comName);
       } else {
         reject();
       }
