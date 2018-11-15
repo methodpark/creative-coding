e503ab5fec7f78eacd01ca10cbe33a69096f2c48
Manuel Ernst <manuel.ernst@methodpark.de>
2018-11-15 07:10:41 +0100

    add error message

--- dmx/lib/getPort.js
+++ dmx/lib/getPort.js
@@ -1,20 +1,18 @@
 const serialport = require('serialport');
 
-function getInterfacePort() {
+function getInterfacePort(manufacturer='FTDI') {
   return new Promise((resolve, reject) => {
     serialport.list((error, ports) => {
       if (error) {
         return reject(error);
       }
     
-      const port = ports.find(port => {
-        return port.manufacturer === 'FTDI';
-      })
-
+      const port = ports.find(port => port.manufacturer === manufacturer);
+      
       if (port) {
         resolve(port.comName);
       } else {
-        reject();
+        reject(new Error(`could not find interface for manufacturer "${manufacturer}"`));
       }
     });
   });
